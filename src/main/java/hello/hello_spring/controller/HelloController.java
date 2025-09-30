34a42971c4a5cf5f4c99a059c411e5cbc028e212
loveIn_JiHo <cale.jiho@gmail.com>
2025-09-30 16:12:22 +0900

    2. 스프링 웹 개발 기초

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,10 +4,44 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HelloController {
+
+    static class Hello {
+      private Data data;
+
+      public Data getData() {
+        return this.data;
+      }
+
+      public void setData(Data data) {
+        this.data = data;
+      }
+    }
+
+    static class Data {
+      private String name;
+      private int age;
+
+      public String getName() {
+        return this.name;
+      }
+
+      public int getAge() {
+        return this.age;
+      }
+
+      public void setName(String name) {
+        this.name = name;
+      }
+
+      public void setAge(int age) {
+        this.age = age;
+      }
+    }
 
     @GetMapping("hello")
     public String hello(Model model) {
@@ -19,5 +53,23 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) { 
       model.addAttribute("name", name);
       return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+      return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name, @RequestParam("age") int age) {
+      Hello hello = new Hello();
+      Data data = new Data();
+      data.setName(name);
+      data.setAge(age);
+
+      hello.setData(data);
+      return hello;
     }
 }
