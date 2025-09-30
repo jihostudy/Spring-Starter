54af6b47b7ac2356c8eb042f61746fa5e5548a0e
loveIn_JiHo <cale.jiho@gmail.com>
2025-09-30 15:51:39 +0900

    정적 컨텐츠와 템플릿 엔진

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -12,5 +13,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!"); // data라는 key에 hello!!를 추가
         return "hello"; // 파일명 : hello.html을 찾아서 렌더링
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { 
+      model.addAttribute("name", name);
+      return "hello-template";
     }
 }
