25f58b3cb0e36c2c1a9a2fc418295e0ae07bdc55
loveIn_JiHo <cale.jiho@gmail.com>
2025-10-01 16:44:46 +0900

    멤버 서비스

---
+++ src/main/java/hello/hello_spring/repository/MemberService.java
@@ -0,0 +1,47 @@
+package hello.hello_spring.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import hello.hello_spring.domain.Member;
+
+public class MemberService {
+  private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+  
+  /**
+   * 회원 가입
+   * @param member
+   * @return
+   */
+  public Long join(Member member) {
+    // 중복 회원 검사
+    validateDuplicateMember(member);
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  /**
+   * 전체 회원 조회
+   * @param member
+   */
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
+
+  /**
+   * 특정 회원 조회
+   * @param member
+   */
+  public Optional<Member> findOne(Long memberId) {
+    return memberRepository.findById(memberId);
+  }
+
+  //중복 회원 검사 메서드
+  private void validateDuplicateMember(Member member) {
+    memberRepository.findByName(member.getName())
+      .ifPresent(m -> {
+        throw new IllegalStateException("이미 존재하는 회원입니다.");
+      });
+  }
+}
