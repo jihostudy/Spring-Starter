5278ed97698a9fe180ee1997d03f12e535b0296f
loveIn_JiHo <cale.jiho@gmail.com>
2025-10-12 17:54:48 +0900

    JdbcTemplateMemberRepository 구현체

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hello_spring;
 
 import hello.hello_spring.repository.JdbcMemberRepository;
+import hello.hello_spring.repository.JdbcTemplateMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 
 import javax.sql.DataSource;
@@ -29,8 +31,9 @@ public class SpringConfig {
   // 직접 스프링빈으로 등록하는 경우, Repository를 변경하기 쉽다 ("교안의 상황에 따라 구현 클래스를 변경해야 하는 경우가 이에해당")
   @Bean
   public MemberRepository memberRepository() {
-    // return new MemoryMemberRepository();
-    return new JdbcMemberRepository(dataSource);
+//    return new MemoryMemberRepository();
+//    return new JdbcMemberRepository(dataSource);
+      return new JdbcTemplateMemberRepository(dataSource);
   }
 
 }
