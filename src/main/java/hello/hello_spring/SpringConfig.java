1f855c153ed32360a263c750b3e8c0c068d6acc6
loveIn_JiHo <cale.jiho@gmail.com>
2025-10-13 17:46:55 +0900

    jpa data Repository 구현체

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -16,28 +16,36 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-  private final DataSource dataSource;
-  private final EntityManager em;
 
+    // JpaRepository를 extends하는 interface를 구현해두면 자동으로 DI
+    private final MemberRepository memberRepository;
 
-  public SpringConfig(DataSource dataSource, EntityManager em) {
-    this.dataSource = dataSource;
-    this.em = em;
-  }
-
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Bean
   public MemberService memberService() {
-    return new MemberService(memberRepository());
+    return new MemberService(memberRepository);
   }
 
-  // 직접 스프링빈으로 등록하는 경우, Repository를 변경하기 쉽다 ("교안의 상황에 따라 구현 클래스를 변경해야 하는 경우가 이에해당")
-  @Bean
-  public MemberRepository memberRepository() {
-//    return new MemoryMemberRepository();
-//    return new JdbcMemberRepository(dataSource);
-//      return new JdbcTemplateMemberRepository(dataSource);
-      return new JpaMemberRepository(em);
-  }
+
+    //  private final DataSource dataSource;
+    //  private final EntityManager em;
+    //
+    //
+    //  public SpringConfig(DataSource dataSource, EntityManager em) {
+    //    this.dataSource = dataSource;
+    //    this.em = em;
+    //  }
+
+    // 직접 스프링빈으로 등록하는 경우, Repository를 변경하기 쉽다 ("교안의 상황에 따라 구현 클래스를 변경해야 하는 경우가 이에해당")
+    //  @Bean
+    //  public MemberRepository memberRepository() {
+    //    return new MemoryMemberRepository();
+    //    return new JdbcMemberRepository(dataSource);
+    //      return new JdbcTemplateMemberRepository(dataSource);
+    //      return new JpaMemberRepository(em);
+    //  }
 
 }
